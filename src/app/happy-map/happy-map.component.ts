39004ab8e07ecffb39d603edbc40defd717b1996
Tabdar Bashir <tabdar.bashir@stud.th-deg.de>
2021-01-30 01:49:32 +0100

    Map components added

---
+++ src/app/happy-map/happy-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-happy-map',
+  templateUrl: './happy-map.component.html',
+  styleUrls: ['./happy-map.component.css']
+})
+export class HappyMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
